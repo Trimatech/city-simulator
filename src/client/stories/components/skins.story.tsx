84057ce357982f3d80e15d6afb90e2c5b877024b
Maigo Erit <maigo.erit@gmail.com>
2025-11-01 22:03:11 +0200

    refactor: replace World2 component with World; update imports across the application for improved consistency and maintainability

--- src/client/stories/components/skins.story.tsx
+++ src/client/stories/components/skins.story.tsx
@@ -4,7 +4,7 @@ import { hoarcekat, useMountEffect } from "@rbxts/pretty-react-hooks";
 import React from "@rbxts/react";
 import { Alerts } from "client/components/alerts";
 import { Menu } from "client/components/menu";
-import { World } from "client/components/world/World2";
+import { World } from "client/components/world/World";
 import { RootProvider } from "client/providers/root-provider";
 import { store } from "client/store";
 import { USER_NAME } from "shared/constants/core";
