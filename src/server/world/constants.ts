5ae7e0ce7b514b9ade527dc89a041ef1d41c4d9d
Maigo Erit <maigo.erit@gmail.com>
2026-03-27 16:37:53 +0200

    fix: update DEFAULT_ORBS value to improve gameplay balance; adjust orbs for non-local environments from 15 to 50

--- src/server/world/constants.ts
+++ src/server/world/constants.ts
@@ -12,7 +12,7 @@ export const SOLDIER_TICK_PHASE = 0;
 export const CANDY_TICK_PHASE = 0.33 * WORLD_TICK;
 export const COLLISION_TICK_PHASE = 0.66 * WORLD_TICK;
 
-export const DEFAULT_ORBS = IS_LOCAL ? 400 : 15; // 15
+export const DEFAULT_ORBS = IS_LOCAL ? 400 : 50;
 
 export const CANDY_LIMITS: { readonly [K in CandyType]: number } = {
 	[CandyType.Default]: 2048,
