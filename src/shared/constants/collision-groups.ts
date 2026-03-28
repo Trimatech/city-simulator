954c124a48b0ae9f7da777264b3d26ce4cf2725b
Maigo Erit <maigo.erit@gmail.com>
2026-03-28 14:37:05 +0200

    add debris collision group

--- src/shared/constants/collision-groups.ts
+++ src/shared/constants/collision-groups.ts
@@ -3,12 +3,14 @@ import { PhysicsService } from "@rbxts/services";
 export const CollisionGroups = {
 	WALL: "Wall",
 	PLAYER: "Player",
+	DEBRIS: "Debris",
 } as const;
 
 export function initiCollisionGroups() {
 	// Register collision groups
 	PhysicsService.RegisterCollisionGroup(CollisionGroups.WALL);
 	PhysicsService.RegisterCollisionGroup(CollisionGroups.PLAYER);
+	PhysicsService.RegisterCollisionGroup(CollisionGroups.DEBRIS);
 
 	// Configure collision rules
 	PhysicsService.CollisionGroupSetCollidable(CollisionGroups.WALL, CollisionGroups.PLAYER, false);
