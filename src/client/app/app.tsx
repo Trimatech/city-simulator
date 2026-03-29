0d460cb0aa41ff97c8f98183d268f32708e3df41
Maigo Erit <maigo.erit@gmail.com>
2026-03-29 22:30:16 +0300

    fix mobile portal

--- src/client/app/app.tsx
+++ src/client/app/app.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "@rbxts/react";
 import { Layer } from "@rbxts-ui/layout";
+import { Frame } from "@rbxts-ui/primitives";
 
 import { Alerts } from "../components/alerts";
 import { BirdCamera } from "../components/camera/BirdCamera";
@@ -12,10 +13,12 @@ import { OverlayScreens } from "../components/OverlayScreens";
 import { Preloader } from "../components/preloader";
 import { Screens } from "../components/Screens";
 import { TopbarScreens } from "../components/TopbarScreens";
+import { PortalProvider } from "../providers/PortalProvider";
 import { World } from "../components/world/World";
 
 export function App() {
 	const mainScreenGuiRef = useRef<ScreenGui>();
+	const portalRef = useRef<Frame>();
 	useEffect(() => {
 		if (mainScreenGuiRef.current) {
 			mainScreenGuiRef.current.ScreenInsets = Enum.ScreenInsets.CoreUISafeInsets;
@@ -24,62 +27,75 @@ export function App() {
 
 	return (
 		<ErrorHandler>
-			<BackgroundMusic />
-			<Preloader />
-			{/* <Voice /> */}
+			<PortalProvider portalRef={portalRef}>
+				<BackgroundMusic />
+				<Preloader />
+				{/* <Voice /> */}
 
-			<Layer>
-				<BirdCamera />
-				<Controller />
-				<World />
-				<ImpactOverlay />
-				<TurboOverlay />
-			</Layer>
+				<Layer>
+					<BirdCamera />
+					<Controller />
+					<World />
+					<ImpactOverlay />
+					<TurboOverlay />
+				</Layer>
 
-			<screengui
-				key="topbar-screens"
-				ResetOnSpawn={false}
-				IgnoreGuiInset
-				ZIndexBehavior="Sibling"
-				DisplayOrder={2}
-				ScreenInsets={Enum.ScreenInsets.TopbarSafeInsets}
-			>
-				<TopbarScreens />
-			</screengui>
+				<screengui
+					key="topbar-screens"
+					ResetOnSpawn={false}
+					IgnoreGuiInset
+					ZIndexBehavior="Sibling"
+					DisplayOrder={2}
+					ScreenInsets={Enum.ScreenInsets.TopbarSafeInsets}
+				>
+					<TopbarScreens />
+				</screengui>
 
-			<screengui
-				key="main-screens"
-				ref={mainScreenGuiRef}
-				ResetOnSpawn={false}
-				IgnoreGuiInset
-				ZIndexBehavior="Sibling"
-				DisplayOrder={3}
-				ScreenInsets={Enum.ScreenInsets.DeviceSafeInsets}
-			>
-				<Screens />
-			</screengui>
+				<screengui
+					key="main-screens"
+					ref={mainScreenGuiRef}
+					ResetOnSpawn={false}
+					IgnoreGuiInset
+					ZIndexBehavior="Sibling"
+					DisplayOrder={3}
+					ScreenInsets={Enum.ScreenInsets.DeviceSafeInsets}
+				>
+					<Screens />
+				</screengui>
 
-			<screengui
-				key="overlay-screens"
-				ResetOnSpawn={false}
-				IgnoreGuiInset
-				ZIndexBehavior="Sibling"
-				DisplayOrder={4}
-				ScreenInsets={Enum.ScreenInsets.None}
-			>
-				<OverlayScreens />
-			</screengui>
+				<screengui
+					key="overlay-screens"
+					ResetOnSpawn={false}
+					IgnoreGuiInset
+					ZIndexBehavior="Sibling"
+					DisplayOrder={4}
+					ScreenInsets={Enum.ScreenInsets.None}
+				>
+					<OverlayScreens />
+				</screengui>
 
-			<screengui
-				key="alerts"
-				ResetOnSpawn={false}
-				IgnoreGuiInset
-				ZIndexBehavior="Sibling"
-				DisplayOrder={10}
-				ScreenInsets={Enum.ScreenInsets.None}
-			>
-				<Alerts />
-			</screengui>
+				<screengui
+					key="portal-target"
+					ResetOnSpawn={false}
+					IgnoreGuiInset
+					ZIndexBehavior="Sibling"
+					DisplayOrder={5}
+					ScreenInsets={Enum.ScreenInsets.None}
+				>
+					<Frame ref={portalRef} size={new UDim2(1, 0, 1, 0)} backgroundTransparency={1} />
+				</screengui>
+
+				<screengui
+					key="alerts"
+					ResetOnSpawn={false}
+					IgnoreGuiInset
+					ZIndexBehavior="Sibling"
+					DisplayOrder={10}
+					ScreenInsets={Enum.ScreenInsets.None}
+				>
+					<Alerts />
+				</screengui>
+			</PortalProvider>
 		</ErrorHandler>
 	);
 }
